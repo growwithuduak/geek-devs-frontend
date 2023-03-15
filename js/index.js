dc76af2494c98fae3875e03364cb5c05c46df4c2
damyl4sure <damyl4sure@gmail.com>
2023-03-14 23:41:17 -0500

    feat: add enter keypress event
    Append name to list after user press the enter key.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const addToInputList = document.querySelector("#addName");
 const addButton = document.querySelector("#addNameToList");
 const addToOutputList = document.querySelector("#outputList");
 
+// Function to append 
 function addToList() {
     const ol = addToOutputList.querySelector("ol");
     const li = document.createElement("li");
@@ -20,8 +21,16 @@ function addToList() {
 };
 
 
+// Execute when the user click on ADD button on Interface
 addButton.addEventListener("click", () => {
-    addToList();
-
-}
+        addToList();
+    }
 );
+
+// Execute when the user presses Enter key on the keyboard
+addToInputList.addEventListener("keypress", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        addToList();
+    }
+  });
