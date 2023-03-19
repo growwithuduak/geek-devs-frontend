5d840ba5ad0921fe63b3467ed8beb8b24633ef2a
Adejuwon-00 <juwonmcalpha@gmail.com>
2023-03-19 19:59:25 +0100

    refactor: added better naming conventions and updated logic for edge case

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-let names=[]
-function myFunction()
-{
- let x=document.getElementById("box");
-  names.push(document.getElementById("name").value);
- x.innerHTML=names.join('<br/><br/>'); 
+const inputField = document.getElementById("name")
+const addBtn = document.getElementById("add-btn")
+const inputNames = document.getElementById("box")
+
+let namesArray = []
+
+inputField.addEventListener('input', function() {
+  addBtn.disabled = inputField.value.trim() === '';
+});
+
+addBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  if (inputField.value.trim() === '') {
+    alert('Please enter a name');
+    return;
+  }
+  console.log('Haha! You added a name')
+  namesArray.push(inputField.value.trim());
+  inputField.value = '';
+  addBtn.disabled = true;
+
+  showNames();
+});
+
+function showNames() {
+  inputNames.innerHTML = '';
+
+  const listNames = document.createElement('ol');
+  namesArray.forEach(function(namesText) {
+    const listData = document.createElement('li');
+    const textNode = document.createTextNode(namesText);
+    listData.appendChild(textNode);
+    listNames.appendChild(listData);
+  });
+
+  inputNames.appendChild(listNames);
 }
 
+
 //for accessibility
-let input = document.getElementById("name");
-input.addEventListener("keypress", function(event) {
+inputField.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
     document.getElementById("add-btn").click();
   }
-}); 
+});
