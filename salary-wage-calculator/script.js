f513dfddbbbe3ff788fb8c2f3bf2a908772fa7d3
Arafat Abdussalam <abdussalamarafat@gmail.com>
2023-03-21 16:53:00 +0100

    feat: define logic responsible for wage calculation

---
+++ salary-wage-calculator/script.js
@@ -0,0 +1,12 @@
+const calculateSalary = (employeeType, salesPerMonth) => {
+    if(salesPerMonth === 15000) {
+        console.log("Salaried Employee")
+    } else if (salesPerMonth === 80000) {
+        console.log("commisioned employee")
+    } else (salesPerMonth === 400000) {
+        console.log("Contracted Employee")
+    }
+};
+
+
+
