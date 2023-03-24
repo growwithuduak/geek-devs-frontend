47831f68ab5572cbd59a3b114eeee05564fa680c
Arafat Abdussalam <abdussalamarafat@gmail.com>
2023-03-24 16:22:47 +0100

    Refactor: change name of parameter passed into checkEmployee function to avoid names confusion

--- salary-calculator/script.js
+++ salary-calculator/script.js
@@ -1,33 +1,38 @@
 const calcMonthlyPay = (employeeType, salesPerMonth) => {
-
+    
+    let salesPerMonth;
     const baseSalary = 150000
 
-    if ((employeeType === "contract" || employeeType === "commissioned") ) {
-        checkEmployeeSalary()
+    if (employeeType === "contract" || employeeType === "commissioned") {
+
+        return checkEmployeeSalary(salesPerMonth);
     } else if (employeeType === "salaried") {
+
         return `$${baseSalary}`;
     } else {
         console.log("You have not entered the employee type")
     }
 };
 
-const checkEmployeeSalary = () => {
-
-    let salesPerMonth;
-
+const checkEmployeeSalary = (salesEarned) => {
     if ("contract") {
-        const calcContract = (0.4 * salesPerMonth);
+        const calcContract = (0.4 * salesEarned);
         return `$${calcContract}`;
-    } else {
-        const calcCommision = ((80000) + (0.085 * salesPerMonth));
+    } else if ("commissioned") {
+        const calcCommision = ((80000) + (0.085 * salesEarned));
         return `$${calcCommision}`;
-    }
-}
+    } else {
+        console.log("not a contract nor commissioned employee")
+    }    
+};
+
 
 console.log(calcMonthlyPay("salaried"))
-console.log(calcMonthlyPay("contract", 200000))
-console.log(calcMonthlyPay("commissioned", 200000))
+console.log(calcMonthlyPay("contract", 243900))
+console.log(calcMonthlyPay("commissioned", 471050))
 console.log(calcMonthlyPay("newSalary"))
 
+
+// to-do:
 // write code that diables the console from displaying "undefined"
-    
+// write code that enables checkEmployeeSalary get the value of salesPerMonth
