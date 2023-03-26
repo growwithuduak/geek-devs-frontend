c520accd797e0af77678b5c829e250c5aa9660e1
Charity <umercy001@gmail.com>
2023-03-26 20:58:20 +0100

    feat/without a frontend interface - v2

--- index.js
+++ index.js
@@ -1,82 +1,52 @@
-const Home = document.querySelector('.home')
-const Total = document.querySelector('.total')
-const select = document.querySelector('select')
-const calculateWages = document.querySelector('.calculate-wages')
-const totalSales = document.querySelector('.total-sales')
-const btn2 = document.querySelector('.btn-2')
-const btn1 = document.querySelector('.btn-1')
-const nameOfEmployee = document.querySelector('.employee-name')
-const commisionedContracted = document.querySelector('.contracted-commisioned')
-const salaryBreakdown = document.querySelector('.salary-breakdown')
-
-
-
-btn1.addEventListener('click', ()=> {
-    if(nameOfEmployee.value != " " && nameOfEmployee.value != null){
-        Home.classList.add('home-page')
-        calculateWages.classList.remove('inactive')
-    }
+const wageCalculator = (employeeType) => {
+          
+    if (employeeType !="" && typeof employeeType === 'string') {
+        if(employeeType === "Salaried" || employeeType === "salaried") {
+            salaried();
     
-})
-
-select.addEventListener ('change', ()=> {
-    Total.innerText = ""
-
-    if(select.value == "Salaried") {
-        commisionedContracted.classList.remove('sales')
-        salaried();
-
-
-    } else if (select.value == "commisioned") {
-        Total.innerHTML = "";
-        salaryBreakdown.innerHTML = ""
-        commisionedContracted.classList.add('sales')
-        commission();
-        totalSales.value = "";
-
-    } else if (select.value == "contract") {
-        Total.innerHTML = "";
-        salaryBreakdown.innerHTML = "";
-        commisionedContracted.classList.add('sales')
-        contracted();
-        totalSales.value = "";
-      
+        } else if (employeeType === "Commissioned" || employeeType === "commissioned") {
+           salesPerMonth = prompt("Enter your total sales for this month: ")
+        
+            commission(salesPerMonth);   
+    
+        } else if (employeeType === "Contracted" || employeeType === "contracted") {
+            salesPerMonth = prompt("Enter your total sales for this month: ")
+            
+            contracted(salesPerMonth);
+          
     } else {
-        commisionedContracted.classList.remove('sales')
+        alert('Employee Type should be either Salaried, Commissioned or Contracted')
     }
+} else {
+    alert('Please Enter a valid Employee type... (Salaried, Commissioned or Contracted) ')
+}
+
+}
+
+const salaried = () => {
     
-})
-
-
-salaried = () => {
-    Total.innerText = nameOfEmployee.value + ' your monthly pay is:  150,000 naira';
+   return 150000;
 }
 
 
-commission = () => {
-    btn2.addEventListener('click', function() {
-        if(totalSales.value != 0 && totalSales != "") {
-            let salesPerMonth = totalSales.value * 0.085;
-            let commissionedSalary = 80000 + salesPerMonth;
-            Total.innerText =  nameOfEmployee.value + ' your monthly pay is: ' + commissionedSalary;
-            salaryBreakdown.innerHTML = ` Here's the summary of your monthly pay:
-             <br/> Base salary = 80,000 
-             <br/> Commission = 8.5% of 
-             ${totalSales.value} `;
-            commisionedContracted.classList.remove('sales')
+const commission = (salesPerMonth) => {
+  
+        if(salesPerMonth >= 0 && salesPerMonth != "") {
+            let commission = salesPerMonth * 0.085;
+            let commissionedSalary = 80000 + commission;
+          return 'Your pay for this month is: ' + commissionedSalary
         } 
-    }) 
 
 }
 
-contracted = () => {
-    btn2.addEventListener('click', function() {
-        if(totalSales.value != 0 && totalSales != "") {
-        let salesPerMonth = totalSales.value * 0.4;
-        Total.innerText = nameOfEmployee.value + ' Your monthly pay is: ' + salesPerMonth;
-        salaryBreakdown.innerHTML =  `You got 40% of 
-        ${totalSales.value} `;
-        commisionedContracted.classList.remove('sales');
-        }
-    })
+const contracted = (salesPerMonth) => {
+    if(salesPerMonth >= 0 && salesPerMonth != "") {
+        let contractSalary = salesPerMonth * 0.4;
+      return('Your pay for this month is: ' + contractSalary)
+    } 
 }
+
+
+
+
+console.log(wageCalculator("salaried"))
