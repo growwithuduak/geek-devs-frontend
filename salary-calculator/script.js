55487511e15975087c2d177ba01db35169f76c7a
Arafat Abdussalam <abdussalamarafat@gmail.com>
2023-03-30 16:04:23 +0100

    Feat: final code restructuring

--- salary-calculator/script.js
+++ salary-calculator/script.js
@@ -1,29 +1,17 @@
 let baseSalary;
 
-const calcMonthlyPay = (employeeType, salesPerMonth) => { 
+const calcMonthlyPay = (employeeType, salesPerMonth) => {  
 
-    validateMonthlyPayError(employeeType, salesPerMonth);
+    validateSalesAndEmployeeType(employeeType, salesPerMonth);
 
-        if (employeeType === "CONTRACT") {
+    if (employeeType === "CONTRACT" || employeeType === "COMMISSIONED") {
 
-            return computeEmployeeSalary(employeeType, salesPerMonth);
-        }; 
-        
-        if (employeeType === "COMMISSIONED") {
-            
-            return computeEmployeeSalary(employeeType, salesPerMonth);
-        }; 
-        
-        if (employeeType === "SALARIED" && typeof(salesPerMonth) === "number") {
-            return "Salaried employees do not depend on sales made per month"
-        };
+        return computeEmployeeSalary(employeeType, salesPerMonth);    
+    }; 
 
-        if (employeeType === "SALARIED") {
-    
-            return computeEmployeeSalary(employeeType, salesPerMonth);    
-        }; 
-
- 
+    if (employeeType === "SALARIED" && !salesPerMonth) {
+        return computeEmployeeSalary(employeeType, salesPerMonth); 
+    }
         
 };
 
@@ -51,43 +39,18 @@ const computeEmployeeSalary = (employeeType, salesPerMonth) => {
     };
 }
 
-const confirmEmployeeType = (employeeType, salesPerMonth) => {
-    if (employeeType !== "SALARIED" || employeeType !== "CONTRACT" || employeeType !== "COMMISIONED") {
 
-        return "You have not entered the required employee type"
-   };
+const validateSalesAndEmployeeType = (employeeType, salesPerMonth) => {
 
-}
-
-const confirmSalesPerMonth = (employeeType, salesPerMonth) => {
-
-   if (typeof(salesPerMonth) !== "number" || salesPerMonth <= 0 ) {
-
-        return "Sales per month must be a number greater than zero"
+    if ((employeeType === "SALARIED") && (typeof(salesPerMonth) === "number" || typeof(salesPerMonth) === "string")) {
+        throw new Error ("Salaried employees do not depend on sales made per month");
     };
 
-    if (!salesPerMonth || typeof(salesPerMonth) === "undefined") {
-        return "This is not the required input format"
-    }
+    if ((employeeType === "COMMISSIONED" || employeeType === "CONTRACT") && (isNaN(salesPerMonth) || salesPerMonth <= 0 )) {
+        throw new Error ("Sales per month must be a number greater than zero");
+    };
 
 };
 
-const validateMonthlyPayError = (employeeType, salesPerMonth) => {
-   
-    confirmEmployeeType(employeeType, salesPerMonth);
-    confirmSalesPerMonth(employeeType, salesPerMonth);
-}
 
-console.log(calcMonthlyPay("SALARIED"))
-console.log(calcMonthlyPay("SALARIED", 200000))
-
-console.log(calcMonthlyPay("CONTRACT", "letters"))
-console.log(calcMonthlyPay("COMMISSIONED", 47103.50))
-console.log(calcMonthlyPay("CONTRACT", -88920))
-
-console.log(calcMonthlyPay("COMMISSIONED", 0));
-console.log(calcMonthlyPay("CONTRACT", 0));
-
-console.log(calcMonthlyPay("newAge"));
-console.log(calcMonthlyPay("COMMISIONED"))
-//console.log(calcMonthlyPay("CONTRACT", y))
+console.log(calcMonthlyPay("SALARIED", 11111))
