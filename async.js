545aa09a002aef6ec1f221921193291efb55787d
damyl4sure <damyl4sure@gmail.com>
2023-04-08 02:08:02 -0400

    feat: async and await promise handling implementation

---
+++ async.js
@@ -0,0 +1,36 @@
+let club1 = "Napoli";
+let club2 = "Juventus";
+
+
+const DelayTime = {
+    TWENTYSECONDS: 20000,
+}
+
+
+
+
+const delayForFirstFixtures = () => {
+    return new Promise(response => {
+        setTimeout(() => {
+            response();
+        }, DelayTime.TWENTYSECONDS);
+    });
+};
+
+
+const firstFixtures = async (a, b) => {
+    try {
+        await delayForFirstFixtures();
+        console.log(a, "vs", b);
+    }catch(err){
+        console.log(err);
+    }
+};
+
+
+//
+console.log(club1);
+
+firstFixtures(club1, club2);
+
+console.log(club2);
