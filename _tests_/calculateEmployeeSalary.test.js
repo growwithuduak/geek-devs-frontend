1a1a2f04511e237d43f37fe8b632facaec49a5c8
nelo_olikeze <neloolikeze@gmail.com>
2023-04-13 21:22:31 +0100

    feat: add a test script to test for edge cases

---
+++ _tests_/calculateEmployeeSalary.test.js
@@ -0,0 +1,43 @@
+ const {
+  employeeTypes,
+  errorMessages,
+  employeeTypeValidation,
+  salesPerMonthValidation,
+  calculateSalariedEmployeeSalary,
+  calculateCommissionedEmployeeSalary,
+  calculateContractEmployeeSalary,
+  calculateEmployeeSalary
+} = require("../calculateEmployeeSalary");
+
+
+test("it calculates the correct salary for a salaried employee", () => {
+  const salary = calculateEmployeeSalary(employeeTypes.SALARIED);
+  const expectedSalary = calculateSalariedEmployeeSalary();
+  expect(salary).toEqual(expectedSalary);
+});
+
+test("it calculates the correct salary for a commissioned employee", () => {
+  const salesPerMonth = 50000;
+  const salary = calculateEmployeeSalary(employeeTypes.COMMISSIONED, salesPerMonth);
+  const expectedSalary = calculateCommissionedEmployeeSalary(salesPerMonth);
+  expect(salary).toEqual(expectedSalary);
+});
+
+test("it calculates the correct salary for a contract  employee", () => {
+  const salesPerMonth = 50000
+  const salary = calculateEmployeeSalary(employeeTypes.CONTRACT, salesPerMonth);
+  const expectedSalary = calculateContractEmployeeSalary(salesPerMonth);
+  expect(salary).toEqual(expectedSalary);
+});
+
+test("it throws an error for invalid employee type", () => {
+  expect(() => employeeTypeValidation("invalid-employee")).toThrow(
+    errorMessages.invalid_employee_type
+  );
+});
+
+test("it throws an error for invalid sales-per-month", () => {
+  expect(() => salesPerMonthValidation(-2)).toThrow(
+    errorMessages.invalid_salesPerMonth
+  );
+});
