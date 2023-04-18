5b0744eb9d4319464bb9786efe47ee093e053374
GDesigns49 <120295473+GDesigns49@users.noreply.github.com>
2023-04-18 08:12:33 +0100

    Added an error message function

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 let inputvalue = document.querySelector("#input_field");
 let addbtn = document.querySelector("#submit_btn");
 let displayname = document.querySelector(".taskList");
+let error = document.querySelector(".error");
 
 let nameList=[]
 let li=document.createElement("li");
 
 addbtn.addEventListener("click", (e)=> {
       e.preventDefault();
-      nameList.push( inputvalue.value);
-      let list = "";
-      for (let index = 0; index < nameList.length; index++) {
-      list += `<li>${nameList[index]}</li>`;
-     
-}
 
-      displayname.innerHTML = list;
-      inputvalue.value = "";
-
-})
-
+      if (inputvalue.value.trim() === "") {
+          error.innerHTML = "Input field cannot be empty!";
+          error.style.color = "red";
+          displayname.insertBefore(error, displayname.firstChild);
+      } else {
+          if (confirm("Do you want to save this input?")) {
+              error.innerHTML = "";
+              nameList.push(inputvalue.value.trim());
+              let list = "";
+              for (let index = 0; index < nameList.length; index++) {
+                  list += `<li>${nameList[index]}</li>`;
+              }
+              displayname.innerHTML = list;
+              inputvalue.value = "";
+          } else {
+              inputvalue.value = "";
+          }
+      }
+});
