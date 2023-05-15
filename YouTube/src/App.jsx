9bddfd290326f6d0cdc50b4a0767a1b21d6b1b46
Arafat Abdussalam <abdussalamarafat@gmail.com>
2023-05-15 19:02:01 +0100

    remove files not needed and start again

--- YouTube/src/App.jsx
+++ YouTube/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react"
-import Header from "./Components/Header/Header"
-import SearchData from "./Components/Header/SearchData"
 
 const App = (props) => {
+
     
 
     return(
         <div>
-            <Header />
-            <SearchData />
+           
         </div>
     )
 
