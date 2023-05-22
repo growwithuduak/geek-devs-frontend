4e2ad86ffa48842e00fedb643ecd07e69c606f01
Charity <umercy001@gmail.com>
2023-05-22 11:04:33 +0100

    feature/Trello_workspace/V1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Button} from './Components/Button'
+
+import './App.css'
+
+function App() {
+
+  return (
+        < div className="myComponent">
+        <p>Workspaces <span>+</span> </p>
+       <Button text = "Grow with Uduak"></Button>
+       <Button text = "Trello Workspace"></Button>
+      
+        </div>
+  )
+}
+
+export default App
