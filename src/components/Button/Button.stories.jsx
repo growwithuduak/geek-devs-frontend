66ca747fd998bebe5f7e95eb6f6aa485b2695ea4
Janet Ogenyi <janeteneogenyi@gmail.com>
2023-07-05 20:33:53 +0100

    change button title for storybook

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -2,7 +2,7 @@ import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
   tags: ['autodocs'],
 };
