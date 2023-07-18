0fd45ea70dfe753f67cf410ba75aa9fe8a68b4fe
Charity <umercy001@gmail.com>
2023-07-18 12:39:22 +0100

    worked on the nav items

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css'
 import { NavBar } from './components/NavBar/NavBar'
 
-import PublicPlaylists from './components/Playlists/PublicPlaylists/PublicPlaylists'
-
 function App() {
   return (
     <>
       <NavBar />
-      <PublicPlaylists />
     </>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './NavBar.css';
 
 
 export const NavBar = () => {
+       const [selectedItem, setSelectedItem] = useState(false);
+
+       const handleClick = (item) => {
+        setSelectedItem(item);
+       }
 
     return (
         <nav className="navbar">
             <div className="logo">
                 <h1> Music App</h1>
             </div>
-            <ul className="nav-items">
-                <li>Contact us</li>
-                <li>Public playlist</li>
-                <li>Login</li>
+            <ul className="nav-items" >
+                <li 
+                   className={selectedItem === 'contact' ? 'active' : ''}
+                   onClick={() => handleClick('contact')}
+                >
+                    Contact us
+                    </li>
+                <li
+                     className={selectedItem === 'playlist' ? 'active' : ''}
+                     onClick={() => handleClick('playlist')}
+                >
+                    Public playlist
+                    </li>
+                <li
+                        className={selectedItem === 'login' ? 'active' : ''}
+                        onClick={() => handleClick('login')}
+                >
+                    Login
+                    </li>
             </ul>
         </nav>
     )
