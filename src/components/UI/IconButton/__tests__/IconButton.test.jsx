184fa81f368a0ca5bfebece6aeda616806f8382f
Arafat Abdussalam <abdussalamarafat@gmail.com>
2023-07-25 14:54:22 +0100

    Test: setup test arrangement

--- src/components/UI/IconButton/__tests__/IconButton.test.jsx
+++ src/components/UI/IconButton/__tests__/IconButton.test.jsx
@@ -1,13 +1,38 @@
-import IconButton from "../IconButton";
 import {render, screen} from "@testing-library/react"
 
+import IconButton from "../IconButton";
 
-describe("Icon Button component", () => {
-    
-    it("renders icon name", () => {
-        render( <IconButton iconName="add-outline"/>)
-        expect().toBeInTheDocument()
-    })
+test ( "should throw an error when button component has no named icon, named size and mode ", () => {
+    render (<IconButton />)
 
+})
+
+test ( "should display button component in primary mode", () => {
+    render (<IconButton />)
+
+})
+
+test ( "should display primary button component with specific named icon", () => {
+    render (<IconButton />)
+
+})
+
+test ( "should display primary Icon button component with medium size ", () => {
+    render (<IconButton />)
+
+})
+
+test ( "should display primary Icon button component with large size ", () => {
+    render (<IconButton />)
+
+})
+
+test ( "should throw an error when specific sized Icon button component is not in primary mode ", () => {
+    render (<IconButton />)
+
+})
+
+test ( "should throw an error when primary Icon button component has no specific size ", () => {
+    render (<IconButton />)
 
 })
